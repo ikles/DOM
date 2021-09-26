691f764b018eb83c3dd888764eef705e3f1b22f1
Igor <membship@ya.ru>
2021-09-26 22:13:00 +0700

    first commit

---
+++ app/result/js/common.js
@@ -0,0 +1,149 @@
+jQuery(document).ready(function( $ ) {
+
+  $(".toggle-mnu").click(function() {
+    $(this).toggleClass("on");
+    $(".top-mnu, .overlay").slideToggle();
+    return false;
+  });
+
+  $('body').click(function () {
+    if( $(".toggle-mnu").hasClass("on") ){
+      $(".toggle-mnu").removeClass("on");
+      $(".top-mnu, .overlay").fadeOut();
+    }
+  });
+
+
+  $(".top-mnu").click(function (e) {
+    e.stopPropagation();
+  });
+
+  $('.a-after').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('rot180');    
+    
+    $(this).parent().next('.top-open').slideToggle(); 
+
+
+  });
+
+
+  $('.filter-btn-hide').click(function (e) {
+    e.preventDefault();
+    if ($(this).find('span').text() == 'Свернуть фильтр') {
+      $(this).find('span').text('Показать фильтр');
+    }
+    else {
+     $(this).find('span').text('Свернуть фильтр');
+   }
+   $(this).find('i').toggleClass('rot180');
+   
+   $('.filter-rows').slideToggle();
+ });
+
+
+  //$("#phone_1").mask("+7 (999) 999-99-99");
+
+//Add Inactive Class To All Accordion Headers
+$('.accordion-header').toggleClass('inactive-header');
+
+  //Set The Accordion Content Width
+  var contentwidth = $('.accordion-header').width();
+  $('.accordion-content').css({'width' : contentwidth });
+  
+  //Open The First Accordion Section When Page Loads
+  $('.accordion-header').first().toggleClass('active-header').toggleClass('inactive-header');
+  $('.accordion-content').first().slideDown().toggleClass('open-content');
+  
+  // The Accordion Effect
+  $('.accordion-header').click(function () {
+    if($(this).is('.inactive-header')) {
+      $('.active-header').toggleClass('active-header').toggleClass('inactive-header').next().slideToggle().toggleClass('open-content');
+      $(this).toggleClass('active-header').toggleClass('inactive-header');
+      $(this).next().slideToggle().toggleClass('open-content');
+    }
+    
+    else {
+      $(this).toggleClass('active-header').toggleClass('inactive-header');
+      $(this).next().slideToggle().toggleClass('open-content');
+    }
+  });
+
+
+  $('.tabs-control-1 .tabs_control_link').click(function (e) {
+    e.preventDefault();
+
+    var item = $(this).closest('.tabs-control-1 .tabs_control_item'),
+    contentItem = $('.tabs_content-1 .tabs_content_item'),
+    itemPosition = item.index();
+
+    contentItem.eq(itemPosition)
+    .add(item)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+
+  });
+
+
+  $('.tabs-control-2 .tabs_control_link').click(function (e) {
+    e.preventDefault();
+
+    var item = $(this).closest('.tabs-control-2 .tabs_control_item'),
+    contentItem = $('.tabs_content-2 .tabs_content_item'),
+    itemPosition = item.index();
+
+    contentItem.eq(itemPosition)
+    .add(item)
+    .addClass('active')
+    .siblings()
+    .removeClass('active');
+
+  });
+
+
+
+
+  if ($('.sl').length) {
+    $('.sl').slick({  
+     dots: false,
+     infinite: true,
+     speed: 300,
+     slidesToShow: 1     
+   });
+  }
+
+/*
+  infinite: true,
+  speed: 300,
+  slidesToShow: 10,
+  slidesToScroll: 1,
+  centerMode: true,
+  autoplay: false,
+  variableWidth: true,
+  autoplaySpeed: 4000,
+  cssEase: 'ease-out',
+  dots: true
+  */
+
+
+
+  $('.link').click(function(e) {
+    $('.modal-overlay_1').fadeIn();
+    e.preventDefault();
+    $('body').addClass('ohi');
+  });
+
+
+  $('.pop-close, .modal-overlay').click(function(e) {
+    e.preventDefault();
+    $('.modal-overlay').fadeOut();
+    $('body').removeClass('ohi');
+  });
+
+
+
+  //$('select').fancySelect();
+
+}); //ready
+
